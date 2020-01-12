0ee776546990506d29a40c7d92736767d873a9b3
princewillzz <princewillz2013@gmail.com>
2020-01-12 21:47:56 +0530

    Created a vote counting app

---
+++ flask/vote1.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+votes = {"yes": 0, "no": 0, "maybe": 0}
+
+@app.route("/")
+def index():
+    return render_template("vote1.html", votes=votes)
+
+@socketio.on("submit vote")
+def vote(data):
+    selection = data["selection"]
+    votes[selection] += 1
+    emit("vote totals", votes, broadcast=True)
+    
+if __name__ == "__main__":
+    socketio.run(app)
+
+
