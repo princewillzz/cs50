a34cb068d5295553f3cbc0332f991cef1e9cb415
princewillzz <princewillz2013@gmail.com>
2020-01-12 21:27:40 +0530

    Created a voting app

---
+++ flask/vote0.py
@@ -0,0 +1,23 @@
+import os
+import requests
+
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("vote0.html")
+
+@socketio.on("submit vote")
+def vote(data):
+    selection = data["selection"]
+    emit("announce vote", {"selection": selection}, broadcast=True)
+    
+if __name__ == "__main__":
+    socketio.run(app)
+
+
